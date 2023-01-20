fb08249c7b52b634b8b0f22d056515bb881353d2
caoyucong <whosmeya@gmail.com>
2023-01-20 14:40:37 +0800

    update

---
+++ cyc-cli-template-vue2/src/App.vue
@@ -0,0 +1,20 @@
+<template>
+  <div id="app">
+    cyc-cli-template-vue2
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App',
+  components: {
+  }
+}
+</script>
+
+<style>
+#app {
+  width: 100%;
+  height: 100%;
+}
+</style>
